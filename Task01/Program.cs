3146b439a3c7d8edac8dda80b330031ad9a902e8
Имя пользователя <dnail@bichikov.ru>
2022-12-01 21:00:57 +0300

    done task all

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+
+    Console.WriteLine($"Да, число {a} является квадратом числа {b}");
+}
+else
+{
+    Console.WriteLine($"Нет, число {a} не является квадратом числа {b}");
+}
---
+++ Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите первое число - ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Sqr(int numb1, int numb2)
+{
+    return numb1 == numb2 * numb2 || numb2 == numb1 * numb1;
+}
+Console.WriteLine("Является ли одно число квадратом второго?");
+string result = Sqr(number1, number2) ? "да" : "нет";
+Console.WriteLine(result);
